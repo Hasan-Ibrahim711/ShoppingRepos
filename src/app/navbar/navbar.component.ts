ec5b6ed0c7833e42521669042aadb83a140ca865
Hasan-Ibrahim711 <hasan.ib938@gmail.com>
2021-07-15 00:49:20 +0300

    AddingHomeIcon

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { AppUser } from './../models/appuser';
 import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { shoppingCard } from '../models/shoppingCard';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faHome } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -16,6 +16,7 @@ export class NavbarComponent implements OnInit {
   shoppingCardCounter?:number;
   card$?:Observable<shoppingCard>;
   fashoppingcard = faShoppingCart;
+  faHome=faHome;
   constructor(public auth: AuthService,public userAuth: UserService,private cardservice: ShoppingCardService) {
     this.userAuth.AppUser$?.subscribe(appuser=> this.appUser=appuser)
    }
